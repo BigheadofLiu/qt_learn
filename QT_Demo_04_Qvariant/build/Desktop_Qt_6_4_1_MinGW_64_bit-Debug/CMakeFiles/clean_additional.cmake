c2ed89e176dcaacb61299fc113c0a24527eb9357
BigheadofLiu <871314635@qq.com>
2025-08-21 11:46:02 +0800

    update

---
+++ QT_Demo_04_Qvariant/build/Desktop_Qt_6_4_1_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\Qt_Demo_04_Qvariant_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Qt_Demo_04_Qvariant_autogen.dir\\ParseCache.txt"
+  "Qt_Demo_04_Qvariant_autogen"
+  )
+endif()
